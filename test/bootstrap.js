7d2dc0e43500da67827b10277b31747637bb65ad
aegypius <innercircle@aegypius.com>
2014-10-03 09:14:46 +0200

    Update Account tests

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -4,23 +4,30 @@ global.chai      = require("chai");
 global.should    = chai.should();
 global.expect    = chai.expect;
 global.assert    = require("assert");
-global.supertest = require("supertest");
+global.request   = require("supertest");
 global.mongoose  = mongoose = require("mongoose");
 global.casual    = casual = require("casual");
-global.debug     = debug = require("debug")('test');
+global.debug     = debug = require("debug")('overseer:test');
 global.port      = process.env.PORT || 3333;
 
-mongoose.connection.on("error", function (err) {
-  debug("error ignored from mongoose : ", err);
-});
-
+global.oauth = {
+  clientId:     "test-client",
+  clientSecret: "test-client-secret"
+};
 
 global.user      = {
   email:    casual.email,
   username: casual.username,
   password: "password",
 };
+
 global.apiKey = {
   keyId:            process.env.TEST_EVEONLINE_API_ID,
   verificationCode: process.env.TEST_EVEONLINE_VERIFICATION_CODE,
 };
+
+mongoose.connection
+  .on("error", function (err) {
+    debug("error ignored from mongoose : ", err);
+  })
+;
