730ddc9dc03f827f12732bbc4591824ad40187d2
aegypius <innercircle@aegypius.com>
2016-02-01 16:18:53 +0100

    Handle cache for styles

--- app/services/apikey.js
+++ app/services/apikey.js
@@ -1,10 +1,11 @@
 angular
-  .module('eve-overseer')
-  .factory('ApiKey', function ($rootScope, $resource) {
-    return $resource('/api/apikeys/:id', { id : '@_id' }, {
-      update: {
-        method: 'PUT'
-      }
+    .module('eve-overseer')
+    .factory('ApiKey', function($rootScope, $resource) {
+        return $resource('/api/apikeys/:id', {
+            id: '@_id'
+        }, {
+            update: {
+                method: 'PUT'
+            }
+        });
     });
-  })
-;
